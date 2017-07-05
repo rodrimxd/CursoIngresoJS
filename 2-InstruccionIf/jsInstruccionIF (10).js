adef0e23f464b8e11de1f72d5403c4b6f1d32ca5
krugalan <krugalan@gmail.com>
2017-07-05 17:49:46 -0300

    2-InstruccionIF v3
    Ultima version de todos los ejercicios completos

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,16 @@
 function Mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+	//Genero el número RANDOM entre 1 y 10 	
+	var a = Math.floor((Math.random() *10 )+ 1);
 	
-
+		if(a >= 9){
+			alert("EXCELENTE");			
+		}
+		else if(a >=4 && a<9){
+			alert("EXCELENTE 2");			
+		}
+		else{
+			alert("EXCELENTE 3");	
+		}
+	
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,5 +1,6 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	Math.floor((Math.random() *10 )+ 1);
+	var a = Math.floor((Math.random() *10 )+ 1);
+	alert(a);
 }//FIN DE LA FUNCIÓN
