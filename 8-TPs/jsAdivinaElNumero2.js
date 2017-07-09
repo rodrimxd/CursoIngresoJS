03b41c1e0257927cab356d7e7b26576197b16d0b
krugalan <krugalan@gmail.com>
2017-07-09 15:54:12 -0300

    Adivina2Complete
    Completo con IF y Switch, en default ramifico una condicion

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -11,18 +11,70 @@ Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
 var contadorIntentos;
+var numero;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
-	
-
+	numeroSecreto = Math.floor((Math.random() *100 )+ 1);
+	//alert(numeroSecreto );
+	console.log(numeroSecreto);
+	contadorIntentos=0;
+	document.getElementById('intentos').value=contadorIntentos;
 }
 
 function verificar()
 {
+	numero=document.getElementById('numero').value;
+	contadorIntentos+=1;
 	
-	
+	if (numero==numeroSecreto) 
+	{
+		switch(contadorIntentos)
+		{
+			case 1:
+				alert("usted es un Psíquico");	
+			break;
 
-}
+			case 2:
+				alert("excelente percepción");	
+			break;
+
+			case 3:
+				alert("Esto es suerte");	
+			break;
+
+			case 4:
+				alert("Excelente técnica");	
+			break;
+
+			case 5:
+				alert("usted está en la media");	
+			break;
+
+			default:
+				if (contadorIntentos>5 && contadorIntentos<11) 
+				{
+					alert("falta técnica");	
+				}
+				else
+				{
+					alert("afortunado en el amor!!");	
+				}
+			break;
+		}
+		document.getElementById('numero').value=0;
+	}
+	else
+	{
+		if (numero>numeroSecreto) 
+		{
+			alert("te pasaste");
+		}
+		else
+		{
+			alert("te falta");
+		}
+	}	
+	document.getElementById('intentos').value=contadorIntentos;
+}	
