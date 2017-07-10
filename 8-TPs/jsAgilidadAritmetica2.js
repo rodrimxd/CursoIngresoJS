234034cd50892af414bd3f1df5a07890d719618b
krugalan <krugalan@gmail.com>
2017-07-10 08:03:41 -0300

    timercomplete

--- 8-TPs/jsAgilidadAritmetica2.js
+++ 8-TPs/jsAgilidadAritmetica2.js
@@ -3,15 +3,76 @@ se agregara un temporizador que
  a los cinco segundos dará por terminado 
  el juego  de no ser ingresado el resultado 
  correcto en ese lapso de tiempo. */
+
+var temporizador=setInterval(fnTempo, 4000);
 var respuesta;
-var temporizador;
+var numero1;
+var numero2;
+var signo;
+var resultado;
+
 function comenzar()
 {
+	numero1=Math.floor((Math.random() *10 )+ 1);
+	document.getElementById('PrimerNumero').value=numero1;
+	numero2=Math.floor((Math.random() *10 )+ 1);
+	document.getElementById('SegundoNumero').value=numero2;
+	signo=Math.floor((Math.random() *4 )+ 1);
+	switch(signo)
+	{
+		case 1:
+			document.getElementById('Operador').value="+";
+			numero1=parseInt(numero1);
+			numero2=parseInt(numero2);
+			resultado=numero1+numero2;
+		break;
+
+		case 2:
+			document.getElementById('Operador').value="-";
+			resultado=numero1-numero2;
+		break;
+
+		case 3:
+			document.getElementById('Operador').value="x";
+			resultado=numero1*numero2;
+		break;
+
+		case 4:
+			document.getElementById('Operador').value="/";
+			resultado=numero1+numero2;
+		break;
+	}
 	
+
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
-	
-
-
+	respuesta=document.getElementById('Respuesta').value;
+	if (respuesta==resultado) 
+	{
+		alert("Es Correcto!!!");		
+	}
+	else
+	{
+		alert("La pifiaste U___U");
+	}
+	//alert(resultado);
 }//FIN DE LA FUNCIÓN
+
+
+function fnTempo() {
+    alert('TIEMPO FUERA!');
+
+    respuesta=document.getElementById('Respuesta').value;
+	if (respuesta==resultado) 
+	{
+		alert("Es Correcto!!!");		
+	}
+	else
+	{
+		alert("ES INCORRECTO!!! U___U");
+	}
+
+    comenzar();
+	document.getElementById('Respuesta').value=null;
+}
