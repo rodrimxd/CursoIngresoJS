80be507f74c1a10b2197ff9a2595fee5097a733a
krugalan <krugalan@gmail.com>
2017-07-09 23:41:56 -0300

    FerreteFacturacionComplete

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,43 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+
+var PrecioUno;
+var PrecioDos;
+var PrecioTres;
+var suma;
+var promedio;
+var precioFinal;
+
 function Sumar () 
 {
-	
+	Precios();	
+	suma=PrecioUno+PrecioDos+PrecioTres;
+	alert(suma);
 }
+
 function Promedio () 
 {
-	
+	Precios();
+	suma=PrecioUno+PrecioDos+PrecioTres;
+	promedio=suma/3;
+	alert(promedio);
 }
+
 function PrecioFinal () 
 {
-	
+	Precios();
+	suma=PrecioUno+PrecioDos+PrecioTres;
+	precioFinal=suma*1.21;
+	alert(precioFinal);
+}
+
+function Precios()
+{
+	PrecioUno=document.getElementById("PrecioUno").value;
+	PrecioDos=document.getElementById("PrecioDos").value;
+	PrecioTres=document.getElementById("PrecioTres").value;
+	PrecioUno=parseInt(PrecioUno);
+	PrecioDos=parseInt(PrecioDos);
+	PrecioTres=parseInt(PrecioTres);
 }
